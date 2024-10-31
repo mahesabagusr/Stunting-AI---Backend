ce400130fdc8e17cc0306d124deb4f609ff685f5
mahesabagusr <mahesabagusraditya1@gmail.com>
2024-10-31 18:28:59 +0700

    add vercel

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import { config } from './src/infra/global_config.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // or '*' for any origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   // res.setHeader('Access-Control-Allow-Origin', '*'); // or '*' for any origin
+//   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   // res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   // next();
+// });
 
 app.use(express.json());
 app.use(router);
