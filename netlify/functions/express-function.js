34469b528c7350af578124467dc946dd657b5a63
mahesabagusr <mahesabagusraditya1@gmail.com>
2024-10-31 21:19:25 +0700

    bugfix - fix netlify config

--- netlify/functions/express-function.js
+++ netlify/functions/express-function.js
@@ -1,5 +1,4 @@
-// netlify/functions/express-app.js
 import serverless from 'serverless-http';
-import app from '../../index.js';
+import app from '../../index.js'; // Import the Express app
 
 export const handler = serverless(app);
